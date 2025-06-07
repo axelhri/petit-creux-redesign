6e898f0fa11ccee9ccd2ed25083ccd90b62e0f38
Axelhri <axelhouairi@gmail.com>
2025-06-07 11:53:30 +0200

    feat(app.js): added errors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import { StatusCodes } from "http-status-codes";
 
 dotenv.config();
 
 const app = express();
+
+import errorHandler from "./middlewares/error-handler.middleware.js";
+import notFound from "./middlewares/not-found.middleware.js";
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(StatusCodes.OK).send("<h1>Petit-Creux API</h1>");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
