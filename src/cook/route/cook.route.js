866694263a47a37a57e5270d405f3308b5947f0e
Axelhri <axelhouairi@gmail.com>
2025-06-07 18:10:44 +0200

    feat(cook): delete cook

--- src/cook/route/cook.route.js
+++ src/cook/route/cook.route.js
@@ -25,4 +25,6 @@ router.put(
   CookService.edit
 );
 
+router.delete("/:id", auth, CookService.remove);
+
 export default router;
--- src/cook/service/cook.service.js
+++ src/cook/service/cook.service.js
@@ -103,9 +103,32 @@ const edit = async (req, res) => {
     bio: cook_bio || cook.cook_bio,
   };
 
-  const updatedUser = await CookController.editCook(id, updatedData);
+  const updatedCook = await CookController.editCook(id, updatedData);
 
-  res.status(StatusCodes.OK).json({ cook: updatedUser });
+  res.status(StatusCodes.OK).json({ cook: updatedCook });
 };
 
-export { register, login, get, edit };
+const remove = async (req, res) => {
+  const { id } = req.params;
+
+  const {
+    rows: [cook],
+  } = await CookController.getCook(id);
+
+  if (!cook) {
+    throw new NotFoundError("Utilisateur introuvable");
+  }
+
+  if (req.cook.cook_id !== cook.cook_id) {
+    throw new UnauthorizedError("Vous ne pouvez pas supprimer ce compte.");
+  }
+
+  const deleteCook = await CookController.deleteCook(id);
+
+  res.status(StatusCodes.OK).json({
+    message: "Utilisateur supprimé avec succès",
+    deleteCook,
+  });
+};
+
+export { register, login, get, edit, remove };
