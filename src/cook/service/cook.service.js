2a25f124f3283b8eb869569085428f41bfa53051
Axelhri <axelhouairi@gmail.com>
2025-06-07 13:24:12 +0200

    feat(cook.service.js): token generation and password hash

--- src/cook/service/cook.service.js
+++ src/cook/service/cook.service.js
@@ -1,21 +1,30 @@
 import * as CookController from "../controller/cook.controller.js";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
   const { cook_name, cook_email, cook_password, cook_profile_picture } =
     req.body;
+
+  const hashedPassword = await bcrypt.hash(cook_password, 10);
 
   const {
     rows: [Cook],
   } = await CookController.register(
     cook_name,
     cook_email,
-    cook_password,
+    hashedPassword,
     cook_profile_picture
   );
+
+  const token = jwt.sign(Cook, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+
   res
     .status(StatusCodes.CREATED)
-    .json({ message: "Inscription réussie.", Cook });
+    .json({ message: "Inscription réussie.", Cook, token });
 };
 
 export { register };
