bb60bc6630a99e2ddbe0331009f8280ad8dd79e1
Axelhri <axelhouairi@gmail.com>
2025-06-09 13:00:20 +0200

    feat(delete): delete image added

--- src/cook/service/delete.service.js
+++ src/cook/service/delete.service.js
@@ -1,5 +1,7 @@
 import * as CookController from "../controller/cook.controller.js";
 import { StatusCodes } from "http-status-codes";
+import { dataUri } from "../../middlewares/multer.js";
+import cloudinary from "../../config/cloudinary.config.js";
 
 const remove = async (req, res) => {
   const { id } = req.params;
@@ -14,6 +16,11 @@ const remove = async (req, res) => {
 
   if (req.cook.cook_id !== cook.cook_id) {
     throw new UnauthorizedError("Vous ne pouvez pas supprimer ce compte.");
+  }
+
+  if (cook.cook_profile_picture) {
+    const publicId = cook.cook_profile_picture.split("/").pop().split(".")[0];
+    await cloudinary.uploader.destroy(`user-profiles/${publicId}`);
   }
 
   const deleteCook = await CookController.deleteCook(id);
