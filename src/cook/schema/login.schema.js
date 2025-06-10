53ef8231b1df78837a46013b80d846f600196dde
Axelhri <axelhouairi@gmail.com>
2025-06-10 12:16:08 +0200

    fix(login.schema): added noempty constraint

--- src/cook/schema/login.schema.js
+++ src/cook/schema/login.schema.js
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 const LoginSchema = z.object({
-  cook_email: z.string().trim().email(),
-  cook_password: z.string().trim(),
+  cook_email: z.string().trim().email().nonempty(),
+  cook_password: z.string().trim().nonempty(),
 });
 
 export default LoginSchema;
