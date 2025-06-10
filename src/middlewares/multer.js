7e99cc9d71c22019b30ecd24e5466213e1ae0ed9
Axelhri <axelhouairi@gmail.com>
2025-06-10 15:42:13 +0200

    fix(cook.routes): changed multer upload

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,7 +3,7 @@ import DatauriParser from "datauri/parser.js";
 import path from "path";
 
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single("cook_profile_picture");
+const multerUploads = multer({ storage });
 const parser = new DatauriParser();
 const dataUri = (req) =>
   parser.format(path.extname(req.originalname).toString(), req.buffer);
